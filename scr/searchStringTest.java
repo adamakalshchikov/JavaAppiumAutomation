888c26a59057deba6100eb6f2e9124833293e825
a.damakalshchikov <a.damakalshchikov@atol.ru>
2020-06-03 01:04:24 +0300

    Добавлен тест searchStringTest

---
+++ scr/searchStringTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class searchStringTest extends FirstTest {
+    @Test
+    public void testSearchStringPresented() {
+        waitForElementAndClick(By.id("org.wikipedia:id/search_container"), "Cannot find input 'Search Wikipedia'", 5);
+
+        WebElement searchStringElement = waitForElementPresent(By.id("org.wikipedia:id/search_src_text"), "Element search_src_text isn't present", 5);
+        String searchText = searchStringElement.getAttribute("text");
+
+        assertEquals("earch…", searchText);
+       }
+}
