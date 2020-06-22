1d16d31889d17734e5cad64cb474c60f51a39ca5
a.damakalshchikov <a.damakalshchikov@atol.ru>
2020-06-23 00:01:31 +0300

    Удалены лишние импорты

--- scr/FirstTest.java
+++ scr/FirstTest.java
@@ -1,14 +1,9 @@
-import io.appium.java_client.TouchAction;
 import lib.CoreTestCase;
 import lib.ui.MainPageObject;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.List;
 
 
 public class FirstTest extends CoreTestCase {
