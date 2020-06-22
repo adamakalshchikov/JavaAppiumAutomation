08542d74db32e072dab97972c3edf2d93fcbff48
a.damakalshchikov <a.damakalshchikov@atol.ru>
2020-06-22 23:37:02 +0300

    Добавил MainPageObject и перенёс туда методы

---
+++ scr/lib/ui/MainPageObject.java
@@ -0,0 +1,130 @@
+package lib.ui;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.List;
+
+public class MainPageObject {
+
+    protected AppiumDriver driver;
+
+    public MainPageObject(AppiumDriver driver) {
+        this.driver = driver;
+    }
+
+    protected WebElement waitForElementPresent(By by, String error_message, long timeOutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    protected WebElement waitForElementPresent(By by, String error_message) {
+        return waitForElementPresent(by, error_message, 5);
+    }
+
+    protected WebElement waitForElementAndClick(By by, String error_message, long timeOutInSeconds) {
+        WebElement element = waitForElementPresent(by, error_message, timeOutInSeconds);
+        element.click();
+        return element;
+    }
+
+    protected WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeOutInSeconds) {
+        WebElement element = waitForElementPresent(by, error_message, timeOutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+
+    protected boolean waitForElementNotPresent(By by, String error_message, long timeOutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
+    }
+
+    protected WebElement waitForElementAndClear(By by, String error_message, long timeOutInSeconds) {
+        WebElement element = waitForElementPresent(by, error_message, timeOutInSeconds);
+        element.clear();
+        return element;
+    }
+
+    protected List<WebElement> waitForAllElements(By by, String errorMessage, long timeOutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        return wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(by));
+    }
+
+    protected boolean waitForElementsAreNotPresented(List<WebElement> elements, String errorMessage,
+            long timeOutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        return wait.until(ExpectedConditions.invisibilityOfAllElements(elements));
+    }
+
+    protected int getAmountOfElements(By by) {
+        List elements = driver.findElements(by);
+        return elements.size();
+    }
+
+    protected void swipeUp(int timeOfSwipe) {
+        TouchAction action = new TouchAction(driver);
+        Dimension size = driver.manage().window().getSize();
+        int x = size.width / 2;
+        int startY = (int) (size.height * 0.8);
+        int endY = (int) (size.height * 0.2);
+
+        action.press(x, startY).waitAction(timeOfSwipe).moveTo(x, endY).release().perform();
+    }
+
+    protected void swipUpQuick() {
+        swipeUp(200);
+    }
+
+    protected void swipeUpToFindElement(By by, String errorMessage) {
+        while (driver.findElements(by).size() == 0) {
+            swipUpQuick();
+        }
+    }
+
+    protected void swipeElementToLeft(By by, String errorMessage) {
+        WebElement elementToSwipe = waitForElementPresent(by, errorMessage);
+
+        int left_x = elementToSwipe.getLocation().getX();
+        int rigt_x = left_x + elementToSwipe.getSize().getWidth();
+        int upper_y = elementToSwipe.getLocation().getY();
+        int lower_y = upper_y + elementToSwipe.getSize().getHeight();
+        int middle_y = (upper_y + lower_y) / 2;
+
+        TouchAction action = new TouchAction(driver);
+
+        action.press(rigt_x, middle_y).waitAction(300).moveTo(left_x, middle_y).release().perform();
+    }
+
+    protected void assertElementNotPresent(By by, String errorMessage) {
+        int amountOfElements = getAmountOfElements(by);
+
+        if (amountOfElements > 0) {
+            String defaultMessage = "An element '" + by.toString() + "' supposed to be not present";
+            throw new AssertionError(defaultMessage + " " + errorMessage);
+        }
+    }
+
+    protected void assertElementPresent(By by, String errorMessage) {
+        int amountOfElements = getAmountOfElements(by);
+
+        if (amountOfElements < 1) {
+            String defaultMessage = "An element '" + by.toString() + "' supposed to be present";
+            throw new AssertionError(defaultMessage + " " + errorMessage);
+        }
+
+    }
+
+    protected String waitForElementAndGetAttribute(By by, String attribute, String errorMessage,
+            long timeOutInSeconds) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeOutInSeconds);
+        return element.getAttribute(attribute);
+    }
+}
