486d8dad9a8f765b1524fb79c166cf15769a58f0
a.damakalshchikov <a.damakalshchikov@atol.ru>
2020-06-22 23:49:38 +0300

    Добавил MainPageObject перед методами в FirstTest

--- scr/FirstTest.java
+++ scr/FirstTest.java
@@ -22,13 +22,13 @@ public class FirstTest extends CoreTestCase {
 
         @Test
         public void testSearch() {
-                waitForElementAndClick(By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                MainPageObject.waitForElementAndClick(By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
                                 "Cannot find input Search Wikipedia", 5);
 
-                waitForElementAndSendKeys(By.xpath("//*[contains(@text, 'Search…')]"), "Java",
+                MainPageObject.waitForElementAndSendKeys(By.xpath("//*[contains(@text, 'Search…')]"), "Java",
                                 "Cannot find search input", 5);
 
-                WebElement element = waitForElementPresent(
+                WebElement element = MainPageObject.waitForElementPresent(
                                 By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']"
                                                 + "//*[@text='Object-oriented programming language']"),
                                 "Cannot find 'Object-oriented programming language'", 15);
@@ -36,34 +36,34 @@ public class FirstTest extends CoreTestCase {
 
         @Test
         public void testCancelSearch() {
-                waitForElementAndClick(By.id("org.wikipedia:id/search_container"),
+                MainPageObject.waitForElementAndClick(By.id("org.wikipedia:id/search_container"),
                                 "Cannot find input 'Search Wikipedia'", 5);
 
-                waitForElementAndSendKeys(By.xpath("//*[contains(@text, 'Search…')]"), "Java",
+                MainPageObject.waitForElementAndSendKeys(By.xpath("//*[contains(@text, 'Search…')]"), "Java",
                                 "Cannot find search input", 5);
 
-                waitForElementAndClear(By.id("org.wikipedia:id/search_src_text"), "Cannot find search field", 5);
+                MainPageObject.waitForElementAndClear(By.id("org.wikipedia:id/search_src_text"), "Cannot find search field", 5);
 
-                waitForElementAndClick(By.id("org.wikipedia:id/search_close_btn"), "Cannot find X cancel btn", 5);
+                MainPageObject.waitForElementAndClick(By.id("org.wikipedia:id/search_close_btn"), "Cannot find X cancel btn", 5);
 
-                waitForElementNotPresent(By.id("org.wikipedia:id/search_close_btn"), "X is still present on the page",
+                MainPageObject.waitForElementNotPresent(By.id("org.wikipedia:id/search_close_btn"), "X is still present on the page",
                                 5);
         }
 
         @Test
         public void testCompareArticleTitle() {
-                waitForElementAndClick(By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                MainPageObject.waitForElementAndClick(By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
                                 "Cannot find input Search Wikipedia", 5);
 
-                waitForElementAndSendKeys(By.xpath("//*[contains(@text, 'Search…')]"), "Java",
+                MainPageObject.waitForElementAndSendKeys(By.xpath("//*[contains(@text, 'Search…')]"), "Java",
                                 "Cannot find search input", 5);
 
-                waitForElementAndClick(
+                MainPageObject.waitForElementAndClick(
                                 By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']"
                                                 + "//*[@text='Object-oriented programming language']"),
                                 "Cannot find element", 5);
 
-                WebElement articleTittleElement = waitForElementPresent(By.id("org.wikipedia:id/view_page_title_text"),
+                WebElement articleTittleElement = MainPageObject.waitForElementPresent(By.id("org.wikipedia:id/view_page_title_text"),
                                 "Article title not found", 15);
 
                 String articleTittle = articleTittleElement.getAttribute("text");
