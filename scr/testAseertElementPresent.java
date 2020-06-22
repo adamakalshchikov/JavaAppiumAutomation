9a98c30228e3e1386a1e6a323d106624881e7f1d
a.damakalshchikov <a.damakalshchikov@atol.ru>
2020-06-23 00:02:06 +0300

    Добавил MainPageObject в testAssertElementPresent

--- scr/testAseertElementPresent.java
+++ scr/testAseertElementPresent.java
@@ -5,15 +5,15 @@ public class testAseertElementPresent extends FirstTest {
 
     @Test
     public void testAseertElementPresented() {
-        waitForElementAndClick(By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+        MainPageObject.waitForElementAndClick(By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
                 "Cannot find input Search Wikipedia", 5);
 
-        waitForElementAndSendKeys(By.xpath("//*[contains(@text, 'Search…')]"), "Java", "Cannot find search input", 5);
+        MainPageObject.waitForElementAndSendKeys(By.xpath("//*[contains(@text, 'Search…')]"), "Java", "Cannot find search input", 5);
 
-        waitForElementAndClick(
+        MainPageObject.waitForElementAndClick(
                 By.xpath("//*[@class='android.widget.TextView' and @text='Java (programming language)']"),
                 "Cannot find 'Java article' in search", 10);
-        assertElementPresent(By.id("org.wikipedia:id/view_page_title_text"), "Cannot find tittle of article");
+        MainPageObject.assertElementPresent(By.id("org.wikipedia:id/view_page_title_text"), "Cannot find tittle of article");
 
     }
 }
