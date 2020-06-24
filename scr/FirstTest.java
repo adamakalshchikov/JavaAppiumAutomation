2dc0c01ea6bbe8eedb12fff98f77a16ff17be4cc
a.damakalshchikov <a.damakalshchikov@atol.ru>
2020-06-25 00:40:59 +0300

    Порефачен testCompareArticleTitle

--- scr/FirstTest.java
+++ scr/FirstTest.java
@@ -1,10 +1,9 @@
 import lib.CoreTestCase;
+import lib.ui.ArticlePageObject;
 import lib.ui.MainPageObject;
 import lib.ui.SearchPageObject;
 
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 
 public class FirstTest extends CoreTestCase {
@@ -39,13 +38,10 @@ public class FirstTest extends CoreTestCase {
                 searchPageObject.initSearchInput();
                 searchPageObject.typeSearchLine("Java");
                 searchPageObject.clickByArticleWithSubstring("Object-oriented programming language");
-                
-                WebElement articleTittleElement = MainPageObject.waitForElementPresent(By.id("org.wikipedia:id/view_page_title_text"),
-                                "Article title not found", 15);
 
-                String articleTittle = articleTittleElement.getAttribute("text");
+                ArticlePageObject articlePageObject = new ArticlePageObject(driver);
+                String articleTittle = articlePageObject.getArticleTitle();
 
                 assertEquals("We see unexpected tittle" + articleTittle, "Java (programming language)", articleTittle);
-
         }
-        }
+}
